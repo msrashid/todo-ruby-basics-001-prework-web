b2539c4c552b8f59becb4b77ed04e43a07604cb1
msrashid <wlitmilkyway@gmail.com>
2016-03-28 23:16:49 +0000

    wassup

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+variable=value
 end
 
-def argue
-  
+def argue(love)
+puts "#{love}"
+  return love
 end
 
-def greeting
-
+def greeting(val, vali)
+puts "The values are #{val} and #{vali}"
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
-
+def pizza_party(food="cheese")
+puts "#{food}"
+  return food
 end
